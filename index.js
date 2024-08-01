6d6f434c573fe473ebb1e171226cc285d5a4a996
Shanmukha <55737606+pshanmukha@users.noreply.github.com>
2024-08-01 19:04:22 +0530

    chore: Add nested resolver functions for reviews, authors, and games

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const resolvers = {
     games(parent, args, contextValue, info) {
       return db.games;
     },
+
     game(_, args) {
       return db.games.find((game) => game.id === args.id);
     },
@@ -22,6 +23,25 @@ const resolvers = {
     },
     author(_, args) {
       return db.authors.find((author) => author.id === args.id);
+    },
+  },
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((review) => review.author_id === parent.id);
+    },
+  },
+  Review: {
+    author(parent) {
+      return db.authors.find((author) => author.id === parent.author_id);
+    },
+
+    game(parent) {
+      return db.games.find((game) => game.id === parent.game_id);
     },
   },
 };
